570267e4c143977b4bffb974bdcb2d3ddfa327f4
dramos <diego.ramos@imagemaker.com>
2020-01-21 12:26:16 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Aplicación de Calculadora';
+  resultadoPadre: number;
+
+  onResultadoSuma(resultado: number) {
+    this.resultadoPadre = resultado;
+
+  }
+}
